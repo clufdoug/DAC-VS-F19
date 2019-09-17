b2c6bcfdfc19b424038ce9bbeed885b7bf981916
Doug Cluff <clufdoug@isu.edu>
2019-09-17 08:51:56 -0600

    Conversion and Error Hadling

---
+++ ClassroomRoomExamples/09-17-19 Example/UserInputForm.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UserInputForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.NumberTextBox = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'NumberTextBox
+        '
+        Me.NumberTextBox.Location = New System.Drawing.Point(116, 46)
+        Me.NumberTextBox.Name = "NumberTextBox"
+        Me.NumberTextBox.Size = New System.Drawing.Size(273, 20)
+        Me.NumberTextBox.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(282, 195)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(153, 59)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Get Input"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(40, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "NumberLabel"
+        '
+        'UserInputForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(486, 287)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.NumberTextBox)
+        Me.Name = "UserInputForm"
+        Me.Text = "Numbers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents NumberTextBox As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ ClassroomRoomExamples/09-17-19 Example/UserInputForm.vb
@@ -0,0 +1,40 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class UserInputForm
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+        Dim resultNumber As Integer
+
+        secondNumber = 7
+
+        'firstNumber = Convert.ToInt32(NumberTextBox.Text)
+
+
+        Try
+            firstNumber = Integer.Parse(NumberTextBox.Text)
+            Console.WriteLine("In the Try Code")
+        Catch ex As Exception
+            Console.WriteLine("In the Catch Code")
+            Console.WriteLine(ex.Message)
+            NumberTextBox.Text = ""
+            MsgBox("Please enter a valid number")
+            NumberTextBox.Select()
+
+        Finally
+            Console.WriteLine("In the Finally Code")
+        End Try
+
+
+
+        resultNumber = firstNumber + secondNumber
+
+        Console.WriteLine(resultNumber)
+
+    End Sub
+
+    Private Sub NumberTextBox_TextChanged(sender As Object, e As EventArgs) Handles NumberTextBox.TextChanged
+
+    End Sub
+End Class
