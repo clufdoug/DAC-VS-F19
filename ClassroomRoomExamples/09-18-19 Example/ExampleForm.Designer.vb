da39637b3e32f1b5d97b622869f9f6dd8b8db5d4
Doug Cluff <clufdoug@isu.edu>
2019-09-18 08:55:39 -0600

    Error handling Examples

---
+++ ClassroomRoomExamples/09-18-19 Example/ExampleForm.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ExampleForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.RunButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(183, 74)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(213, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(183, 100)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(213, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(183, 126)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(213, 20)
+        Me.TextBox3.TabIndex = 2
+        '
+        'RunButton
+        '
+        Me.RunButton.Location = New System.Drawing.Point(206, 296)
+        Me.RunButton.Name = "RunButton"
+        Me.RunButton.Size = New System.Drawing.Size(75, 23)
+        Me.RunButton.TabIndex = 3
+        Me.RunButton.Text = "Run Button"
+        Me.RunButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(354, 296)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 23)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ExampleForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.RunButton)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "ExampleForm"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents RunButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ ClassroomRoomExamples/09-18-19 Example/ExampleForm.vb
@@ -0,0 +1,80 @@
+﻿Option Strict On
+Option Explicit On
+Public Class ExampleForm
+    Private Sub RunButton_Click(sender As Object, e As EventArgs) Handles RunButton.Click
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+        Dim errorMessage As String
+        errorMessage = ""
+
+        LoadTestData() ' Load sample data, Remove for production
+
+        'validate user input in reverse tabstop order
+        Try
+            firstNumber = Integer.Parse(TextBox1.Text)
+        Catch anyException As Exception
+            'Exception thrown so do stuff
+            errorMessage = errorMessage & "Please enter a number in the 1st Textbox" & vbNewLine
+            TextBox1.Select()
+            TextBox1.Clear()
+        End Try
+
+        Try
+
+            secondNumber = Integer.Parse(TextBox2.Text)
+        Catch anyException As Exception
+            'Exception thrown so do stuff
+            errorMessage = errorMessage & "Please enter a number in the 2nd Textbox" & vbNewLine
+            TextBox2.Select()
+            TextBox2.Clear()
+        End Try
+
+        If errorMessage <> "" Then
+            MessageBox.Show(errorMessage, "We have a Problem")
+        End If
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub LoadTestData()
+        Dim sample As Integer
+
+        For sample = 1 To 5 Step 1
+            Select Case sample
+                Case = 1
+
+                    TextBox1.Text = "5"
+                    TextBox2.Text = "6"
+                    Exit For
+                Case = 2
+                    TextBox1.Text = "5"
+                    TextBox2.Text = "bad"
+                    Exit For
+                Case = 3
+                    TextBox1.Text = "bad"
+                    TextBox2.Text = "6"
+                    Exit For
+                Case = 4
+                    TextBox1.Text = "bad"
+                    TextBox2.Text = "bad"
+                    Exit For
+                Case = 5
+                    TextBox1.Text = "5"
+                    TextBox2.Text = "6"
+
+            End Select
+        Next sample
+
+
+
+    End Sub
+
+
+
+
+
+
+End Class
