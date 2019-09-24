2b7a72a0ddabe2590b4a259dedd360378a0e7c23
Doug Cluff <clufdoug@isu.edu>
2019-09-24 08:51:33 -0600

    9-24 Example

---
+++ ClassroomRoomExamples/09-23-19 Example/Form1.vb
@@ -0,0 +1,32 @@
+﻿
+
+Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim x As Long
+
+        Dim y As Long
+        'x = 20
+        'y = 7
+
+        ''returns the decimal value of the division
+        'Console.WriteLine("x/y =" & Str(x / y))
+        ''returns the integer (whole number) value
+        'Console.WriteLine("x/y =" & Str(x \ y))
+        ''returns the remainder value after division
+        'Console.WriteLine("x/y =" & Str(x Mod y))
+
+
+        x = 8
+        x <<= 1
+
+        Console.WriteLine(Str(x))
+        x >>= 1
+        Console.WriteLine(Str(x))
+        'x = x + 1
+        '
+    End Sub
+
+End Class
