7cf172cf715b5f61b26a891942df62bb8a260f87
Doug Cluff <clufdoug@isu.edu>
2019-11-18 20:14:48 -0700

    4-6 Updates

---
+++ Assignments/Asg4-6/rentalForm.vb
@@ -0,0 +1,52 @@
+﻿'Doug Cluff
+'RCET0265
+'Asg4-1
+'Math Contest Form
+'https://github.com/clufdoug/DAC­VS­F19/tree/master/Assignments/Asg4-6
+
+
+Option Strict On
+Option Explicit On
+
+
+
+Public Class rentalForm
+
+
+
+    Private Sub calculateButton_Click(sender As Object, e As EventArgs) Handles calculateButton.Click
+
+    End Sub
+
+    Private Sub clearButton_Click(sender As Object, e As EventArgs) Handles clearButton.Click
+
+    End Sub
+
+    Private Sub summaryButton_Click(sender As Object, e As EventArgs) Handles summaryButton.Click
+
+    End Sub
+
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+
+    End Sub
+
+    Private Sub milesDriven()
+
+    End Sub
+
+    Private Sub mileCharge()
+
+    End Sub
+
+    Private Sub dayCharge()
+
+    End Sub
+
+    Private Sub discount()
+
+    End Sub
+
+    Private Sub youOwe()
+
+    End Sub
+End Class
